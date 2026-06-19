9ea83fc186a930be1b446ea9f940e767d4a26142
nermerm <nastyaermak008@gmail.com>
2026-06-19 22:08:28 +0300

    Update5 solution.c

--- problem4-sockets-tcp-client/solution.c
+++ problem4-sockets-tcp-client/solution.c
@@ -22,7 +22,28 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Usage: %s <ipv4_addr> <port>\n", argv[0]);
         return 1;
     }
+    
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in pupu;
+    memset(&pupu, 0, sizeof(pupu));
+    pupu.sin_family = AF_INET;
+    pupu.sin_port = htons(atoi(argv[2]));
 
+    inet_aton(argv[1], &pupu.sin_addr);
+
+    connect(sockfd, &pupu, sizeof(pupu));
+    char buffer[128];
+    int num;
+    int ans;
+    while (fgets(buffer, sizeof(buffer), stdin) != NULL) {
+        num = atoi(buffer);
+        send(sockfd, &num, sizeof(num), 0);
+        recv(sockfd, &ans, sizeof(ans), 0);
+        printf("%d\n", ans);
+        fflush(stdout);
+    }
+    
+    close(sockfd);
     // TODO: создайте TCP-сокет (AF_INET, SOCK_STREAM),
     //       заполните struct sockaddr_in с помощью inet_aton/inet_pton,
     //       подключитесь через connect,
