526e5ea1a84368f3ede8f507550504732185cc0c
nermerm <nastyaermak008@gmail.com>
2026-06-19 21:21:30 +0300

    Update3 solution.c

--- problem2-exec-exec-python/solution.c
+++ problem2-exec-exec-python/solution.c
@@ -8,10 +8,15 @@
 // Использовать дополнительные процессы запрещено — нужно использовать exec.
 
 int main(void) {
+    char in_put[512];
+    fgets(in_put, sizeof(in_put), stdin);
+    char script[512];
+    snprintf(script, sizeof(script), "print(%s)", in_put);
+    char *args[] = {"python3", "-c", script, NULL};
+    execvp("python3", args);
     // TODO: прочитайте выражение из stdin,
     //       затем вызовите execvp/execlp для запуска python3,
     //       который вычислит и выведет результат.
     //       Подсказка: python3 -c "print(<выражение>)"
-
     return 0;
 }
